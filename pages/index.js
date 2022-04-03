16fc82a01278c653f2ecf9f6149103db6de2af3c
Mauricio Fullana <mfullana4@gmail.com>
2022-04-03 19:35:21 -0300

    Adding components with dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,43 @@
 import { Fragment } from 'react';
+import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2022-02-15',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2022-02-15',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2022-02-15',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2022-02-15',
+  },
+];
 
 function HompePage() {
   return (
     <Fragment>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
