1d937f1da40d134c4e76af33eb735bae4926a1e4
Mauricio Fullana <mfullana4@gmail.com>
2022-04-03 19:44:11 -0300

    Adding all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,38 @@
-function AllPostsPage() {}
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2022-02-15',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2022-02-15',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2022-02-15',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2022-02-15',
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
